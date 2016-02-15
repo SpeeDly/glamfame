1096ed1e31d32d169fcb4150068489c6e7755beb
SpeeDly <zhuhov@gmail.com>
2016-02-15 19:09:35 +0200

    small fixed for the new server

--- prepare.sh
+++ prepare.sh
@@ -4,11 +4,11 @@ GENV=/home/ENV/glamfame/bin/activate
 #source $GENV && killall gunicorn;
 cd $GHOME && git reset --hard;
 cd $GHOME && git pull;
-find -type d -name migration -exec rm -rf {} $GHOME;
+find -type d -name migration -exec rm -rf {} $GHOME\;
 cd $GHOME && git pull;
 source $GENV && cd $GHOME && python manage.py reset_db;
 source $GENV && cd $GHOME && python manage.py syncdb;
 source $GENV && cd $GHOME && python manage.py rebuild_index --noinput;
 source $GENV && cd $GHOME && python manage.py collectstatic;
-source $GENV && cd $GHOME && python manage.py python manage.py loaddata dumpdata.json;
-source $GENV && cd $GHOME && python manage.py python manage.py migrate django_cron;
+source $GENV && cd $GHOME && python manage.py loaddata dumpdata.json;
+source $GENV && cd $GHOME && python manage.py migrate django_cron;
